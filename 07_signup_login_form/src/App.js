e0b0d09fb622fe0bae04268f4d84b1b386fa01eb
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-11-29 13:32:13 +0530

    add: 07_signup_login_form project

---
+++ 07_signup_login_form/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import SignupLogin from "./components/SignupLogin/SignupLogin";
+function App() {
+  return (
+    <div className="App">
+      <SignupLogin />
+    </div>
+  );
+}
+
+export default App;
