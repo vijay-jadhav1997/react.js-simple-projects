611965fc9b3a9e9afeb0987b088f86a540f80776
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-11-29 13:33:08 +0530

    update 07_signup_login_form project

---
+++ 07_signup_login_form/src/components/SignupLogin/SignupLogin.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./SignupLogin.css";
+import user from "../Assets/user.png";
+import email from "../Assets/email.png";
+import padlock from "../Assets/padlock.png";
+import eye from "../Assets/eye.png";
+import hidden from "../Assets/hidden.png";
+
+const SignupLogin = () => {
+  const [action, setAction] = useState("sign up");
+  const [pswdIcon, setPswdIcon] = useState({ hidden });
+  return (
+    <div className="SignupLogin">
+      <div className="header">
+        <h1 className="title">{action}</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user} alt="" />
+          <input type="text" placeholder="Name..." />
+        </div>
+        {action !== "sign up" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={email} alt="" />
+            <input type="email" placeholder="Email Id..." />
+          </div>
+        )}
+        <div className="input passwordBox">
+          <img src={padlock} alt="" />
+          <input
+            type={pswdIcon === hidden ? "password" : "text"}
+            placeholder="Password..."
+          />
+          <div
+            className="hide-show-icon"
+            onClick={() =>
+              pswdIcon === hidden ? setPswdIcon(eye) : setPswdIcon(hidden)
+            }
+          >
+            <img src={pswdIcon} alt="" />
+          </div>
+        </div>
+        {action === "sign up" ? (
+          <div></div>
+        ) : (
+          <div className="forgotPswdBox">
+            <p className="forgotPswd">
+              Forgot password? <span>click here !</span>
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="submitBtns-Container">
+        <button
+          type="button"
+          className={action === "sign up" ? "" : "gray"}
+          onClick={() => setAction("sign up")}
+        >
+          sign up
+        </button>
+        <button
+          type="button"
+          className={action === "login" ? "" : "gray"}
+          onClick={() => setAction("login")}
+        >
+          login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignupLogin;
