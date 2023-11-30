a23bd4d73bd9ee775264cc419cb63ba9ade80f9e
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-11-30 16:05:20 +0530

    create "Weather_App" simple react project

---
+++ 09_weather_app/src/WeatherApp.jsx
@@ -0,0 +1,13 @@
+import "./WeatherApp.css";
+import CityWeather from "./components/ticTacToe/CityWeather";
+
+function WeatherApp() {
+  return (
+    <>
+      <h1>!! Jay Jay Ram Krushna Hari !!</h1>
+      <CityWeather />
+    </>
+  );
+}
+
+export default WeatherApp;
---
+++ 09_weather_app/src/components/CityWeather/CityWeather.jsx
@@ -0,0 +1,11 @@
+import "./CityWeather.css";
+
+const CityWeather = () => {
+  return (
+    <div className="ticTacToeContainer">
+      <h2>!! Shree Dnyanoba Mauli Tukaram !!</h2>
+    </div>
+  );
+};
+
+export default CityWeather;
