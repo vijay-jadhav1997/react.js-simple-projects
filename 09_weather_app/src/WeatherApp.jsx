ae8d98ab69170e14336d22100a39bdd01d0bfc4a
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-08 14:13:43 +0530

    style: styling of weather app completed

--- 09_weather_app/src/WeatherApp.jsx
+++ 09_weather_app/src/WeatherApp.jsx
@@ -3,10 +3,13 @@ import CityWeather from "./components/CityWeather";
 
 function WeatherApp() {
   return (
-    <div className="">
-      <h1>
-        Weather App using <span>React</span>
-      </h1>
+    <div className="bg-blend-multiply bg-blue-950 w-screen h-[1000px] pt-10">
+      <header className="bg-white text-cyan-600 text-3xl md:text-4xl font-bold w-max px-10 md:px-12 py-3 md:py-4 mx-auto mb-3 rounded-full">
+        Weather App using{" "}
+        <span className="underline underline-offset-4 decoration-wavy decoration-sky-700 text-pink-700 text-4xl md:text-5xl">
+          React
+        </span>
+      </header>
       <CityWeather />
     </div>
   );
--- 09_weather_app/src/components/CityWeather.jsx
+++ 09_weather_app/src/components/CityWeather.jsx
@@ -40,8 +40,8 @@ const CityWeather = () => {
   // let cityDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=a73d68f9bcb2329ef697a7b1f304b10b`;
 
   return (
-    <div className="bg-sky-950 text-white pt-5 rounded-md ">
-      <div className="search_bar bg-pink-500 flex justify-center">
+    <div className="bg-cyan-900 text-white rounded-lg mx-auto mt-10 md:w-max w-[420px] px-5 md:py-10 py-5">
+      <div className="search_bar flex justify-center mx-auto w-[90%] max-w-2xl">
         <input
           type="text"
           name=""
@@ -49,7 +49,7 @@ const CityWeather = () => {
           onChange={(e) => {
             return setCityName(e.target.value);
           }}
-          className=" outline-none rounded-full min-w-[40%] indent-5 text-lg"
+          className=" outline-none rounded-full w-full indent-5 text-lg"
           placeholder="Search..."
         />
         <button
@@ -62,54 +62,58 @@ const CityWeather = () => {
           <img src={search} alt="" className="w-5" />
         </button>
       </div>
-      <div className="cityWeather mt-5 flex flex-col md:flex-row">
-        <div className="weatherBox1 border">
-          <div className="date_time">
-            <div className="cityName">
-              <img className="w-5 inline" src={location} alt="" />
-              <span className="text-2xl">Alandi (devachi)</span>
-            </div>
-            <p className="text-gray-300 text-lg mt-10">September 15</p>
-            <p>Monday</p>
+      <div className="cityWeather md:mt-10 mt-3 flex flex-col md:flex-row items-center">
+        <div className="weatherBox1 pb-5 mb-2 flex flex-wrap justify-center md:block">
+          <div className="cityName flex items-center gap-x-2">
+            <img className="w-5 inline" src={location} alt="" />
+            <span className="md:text-3xl text-2xl font-medium">
+              Alandi (devachi)
+            </span>
           </div>
-          <div className="temperatureBox">
-            <img className="w-16" src={temperature} alt="" />
-            <div className="temp">
-              <p>25° C</p>
-              <p>Feels like 23° C</p>
+          <div className="date_time ml-10 md:mt-10 mt-5 text-xl">
+            <p className="text-gray-300">September 15</p>
+            <p className="text-gray-300">Monday</p>
+          </div>
+          <div className="temperatureBox flex md:mt-10 mt-5">
+            <img className="w-14 h-12" src={temperature} alt="" />
+            <div className="temp ml-2">
+              <p className="text-5xl font-semibold">25° C</p>
+              <p className="text-gray-200 mt-2 text-lg font-medium">
+                Feels like 23° C
+              </p>
             </div>
           </div>
         </div>
-        <div className="weatherBox2 border px-2 ">
-          <img className="w-52" src={cloudy} alt="weather image" />
+        <div className="weatherBox2 px-2">
+          <img className="md:w-56 w-36" src={cloudy} alt="weather image" />
           <p className="text-lg mt-3 text-center py-2 font-semibold">Cloudy</p>
         </div>
-        <div className="weatherBox3 border px-5 pt-10 pb-2">
-          <div className="sunrise my-1 py-[2px] px-3">
+        <div className="weatherBox3 px-2 md:py-5 pb-2 md:ml-8 md:flex-col flex  flex-wrap gap-x-8">
+          <div className="sunrise my-1 py-1 px-1">
             <img className="w-8 h-6 inline" src={sunrise} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
               sunrise 7:00
             </p>
           </div>
-          <div className="sunset my-1 py-[2px] px-3">
+          <div className="sunset my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={sunset} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
               sunset 18:33
             </p>
           </div>
-          <div className="humidity my-1 py-[2px] px-3">
+          <div className="humidity my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={humidity} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
               64% Humidity
             </p>
           </div>
-          <div className="windSpeed my-1 py-[2px] px-3">
+          <div className="windSpeed my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={windy} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              18 km/h Wind Speed
+              18 km/h Wind
             </p>
           </div>
-          <div className="pressure my-1 py-[2px] px-3">
+          <div className="pressure my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={pressure} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
               1011 mb pressure
