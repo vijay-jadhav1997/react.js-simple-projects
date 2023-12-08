7bbaf3e42b4eea95bf63cddf2713ae6239a4b4a0
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-08 20:52:16 +0530

    Update CityWeather.jsx
    Update CityWeather Component to make our Weather App functional and get live weather data of city by search with city name.

--- 09_weather_app/src/components/CityWeather.jsx
+++ 09_weather_app/src/components/CityWeather.jsx
@@ -8,34 +8,43 @@ import pressure from "./assets/pressure.png";
 import location from "./assets/location.png";
 import cloudy from "./assets/cloudy.png";
 import { useState } from "react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
+
 const CityWeather = () => {
   const [cityWeather, setCityWeather] = useState({});
-  const [cityUrl, setCityUrl] = useState(``);
-  const [cityName, setCityName] = useState("Pune");
+  const [cityUrl, setCityUrl] = useState("");
+  const [cityName, setCityName] = useState("");
 
-  // useEffect(() => {
-  //   fetchCityData();
-  // }, []);
+  useEffect(() => {
+    fetchCityData();
+  }, [cityUrl]);
 
   async function fetchCityData() {
     try {
       const cityData = await fetch(cityUrl);
       const jsonData = await cityData.json();
       setCityWeather(jsonData);
-      console.log(cityData);
-      console.log(cityWeather);
+      // console.log(cityData);
     } catch (error) {
       console.log(error);
     }
   }
 
   const searchCityWeather = () => {
+    let cityNameOk =
+      cityName.split("")[0].toUpperCase() +
+      cityName.substring(1, cityName.length).toLowerCase();
+    // console.log(cityNameOk);
     setCityUrl(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=Metric&appid=a73d68f9bcb2329ef697a7b1f304b10b`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityNameOk}&units=Metric&appid=a73d68f9bcb2329ef697a7b1f304b10b`
     );
     fetchCityData();
   };
+
+  console.log(cityWeather);
+
+  const { name, sys, weather, wind, main } = cityWeather;
+
   // let api_key = "a73d68f9bcb2329ef697a7b1f304b10b";
   // let cityDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=a73d68f9bcb2329ef697a7b1f304b10b`;
 
@@ -49,14 +58,14 @@ const CityWeather = () => {
           onChange={(e) => {
             return setCityName(e.target.value);
           }}
-          className=" outline-none rounded-full w-full indent-5 text-lg"
+          className=" outline-none rounded-full w-full indent-5 text-lg text-cyan-950"
           placeholder="Search..."
         />
         <button
           type="button"
           className="ml-5 px-3 py-3 rounded-full bg-white hover:bg-slate-200"
           onClick={() => {
-            searchCityWeather;
+            searchCityWeather();
           }}
         >
           <img src={search} alt="" className="w-5" />
@@ -66,9 +75,7 @@ const CityWeather = () => {
         <div className="weatherBox1 pb-5 mb-2 flex flex-wrap justify-center md:block">
           <div className="cityName flex items-center gap-x-2">
             <img className="w-5 inline" src={location} alt="" />
-            <span className="md:text-3xl text-2xl font-medium">
-              Alandi (devachi)
-            </span>
+            <span className="md:text-3xl text-2xl font-medium">{name}</span>
           </div>
           <div className="date_time ml-10 md:mt-10 mt-5 text-xl">
             <p className="text-gray-300">September 15</p>
@@ -77,9 +84,11 @@ const CityWeather = () => {
           <div className="temperatureBox flex md:mt-10 mt-5">
             <img className="w-14 h-12" src={temperature} alt="" />
             <div className="temp ml-2">
-              <p className="text-5xl font-semibold">25° C</p>
+              <p className="text-5xl font-semibold">
+                {Math.round(main?.temp)}° C
+              </p>
               <p className="text-gray-200 mt-2 text-lg font-medium">
-                Feels like 23° C
+                Feels like {Math.round(main?.feels_like)}° C
               </p>
             </div>
           </div>
@@ -92,31 +101,31 @@ const CityWeather = () => {
           <div className="sunrise my-1 py-1 px-1">
             <img className="w-8 h-6 inline" src={sunrise} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              sunrise 7:00
+              sunrise {sys?.sunrise}
             </p>
           </div>
           <div className="sunset my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={sunset} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              sunset 18:33
+              sunset {sys?.sunset}
             </p>
           </div>
           <div className="humidity my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={humidity} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              64% Humidity
+              {main?.humidity}% Humidity
             </p>
           </div>
           <div className="windSpeed my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={windy} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              18 km/h Wind
+              {wind?.speed} km/h Wind
             </p>
           </div>
           <div className="pressure my-1 py-[2px] px-1">
             <img className="w-8 h-6 inline" src={pressure} alt="" />
             <p className="inline-block ml-3 text-lg text-gray-300">
-              1011 mb pressure
+              {main?.pressure} mb pressure
             </p>
           </div>
         </div>
