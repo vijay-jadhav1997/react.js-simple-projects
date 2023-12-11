0c940e85595f57134b596deec56c94fb3c3cdc0f
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-11 10:33:19 +0530

    style: style Quiz component

--- 10_quiz_app/src/components/Header.js
+++ 10_quiz_app/src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <h1>Quiz App using React</h1>
+      <h1>✨ Quiz App ✨</h1>
     </header>
   );
 };
--- 10_quiz_app/src/components/Quiz.js
+++ 10_quiz_app/src/components/Quiz.js
@@ -2,7 +2,7 @@ import { Questions } from "../helpers/QuestionBank";
 import { useState } from "react";
 
 const Quiz = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion] = useState(0);
   return (
     <div className="quiz">
       <h1 className="question">{Questions[currentQuestion]?.question}</h1>
@@ -11,6 +11,10 @@ const Quiz = () => {
         <button>{Questions[currentQuestion]?.optionB}</button>
         <button>{Questions[currentQuestion]?.optionC}</button>
         <button>{Questions[currentQuestion]?.optionD}</button>
+      </div>
+      <div className="buttonsBox">
+        <button className="skipBtn">Skip</button>
+        <button className="nextBtn">Next</button>
       </div>
     </div>
   );
