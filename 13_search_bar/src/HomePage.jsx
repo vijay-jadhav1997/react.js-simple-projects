166b7bb4ef3db924c05409a581ae64bae0b04b94
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-11 19:22:57 +0530

    create '13_search_bar' as new react project

---
+++ 13_search_bar/src/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+import SearchBar from "./components/SearchBar";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default HomePage;
