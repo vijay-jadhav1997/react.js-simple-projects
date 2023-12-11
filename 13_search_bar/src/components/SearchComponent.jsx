f8eb1354d50a9a6d370eb6c6ee66f28ee5bf814f
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-11 20:43:02 +0530

    style: style 13_search_bar project

--- 13_search_bar/src/components/SearchComponent.jsx
+++ 13_search_bar/src/components/SearchComponent.jsx
@@ -1,5 +1,16 @@
+import "./searchComponent.css";
+
 const SearchComponent = ({ data }) => {
-  return <div className="searchComponent">{data?.name}</div>;
+  return (
+    <div
+      className="searchComponent"
+      onClick={() => {
+        alert(`You have clicked ${data?.name}`);
+      }}
+    >
+      {data?.name}
+    </div>
+  );
 };
 
 export default SearchComponent;
