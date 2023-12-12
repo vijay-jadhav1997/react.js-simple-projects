facec9a4bc922697617b1a7f5e29ac694a6b6a89
vijay-jadhav1997 <vijay05111997@gmail.com>
2023-12-12 20:22:02 +0530

    create '15_news_app' React Project

---
+++ 15_news_app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import NewsBoard from "./components/NewsBoard";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <NewsBoard />
+    </div>
+  );
+}
+
+export default App;
