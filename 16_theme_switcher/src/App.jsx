7c94ef50687617f8cab0baac35c0828839c00c47
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-01 20:02:22 +0530

    add 16_theme_switcher

---
+++ 16_theme_switcher/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center gap-5">
+      This is Simple React App
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ 16_theme_switcher/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="text-2xl font-semibold">
+      This is simple Header of our App.
+    </div>
+  );
+};
+
+export default Header;
