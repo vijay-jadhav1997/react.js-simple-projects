e408ae455f341e2286ffe9e3456a5d420cebb33d
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-05 19:29:22 +0530

    create Navbar component & add to 18_user_table app

---
+++ 18_user_table/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <p>Welcome, to learn Redux Toolkit with React</p>
+      <h1>ADMIN TABLE</h1>
+      <p>
+        Redux Toolkit : A solution to manage global state and store date
+        globally, to make it available to every component of our react app.
+      </p>
+      <div className="navbar">
+        <div>HOME</div>
+        <div>ABOUT</div>
+        <div>PROJECTS</div>
+        <div>CODE</div>
+        <div>CONTACT</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
