5b696b5f51d1701f95e69e006b5dc31898d91485
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-12 14:45:34 +0530

    Update Navbar.jsx

--- 18_user_table/src/components/Navbar.jsx
+++ 18_user_table/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 // import React from 'react'
+import ReactLOGO from "../assets/react.svg";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <p>✨ Welcome, to learn Redux Toolkit with React...!</p>
+      <p>
+        ✨ Welcome, to learn Redux Toolkit with React...!{" "}
+        <img src={ReactLOGO} style={{ width: "20px", height: "20px" }} alt="" />
+      </p>
       <h1 className="heading">: ADMIN TABLE :</h1>
       <p>
         Redux Toolkit : A solution to manage global state and store data
         globally, to make it available to every required component of our react
-        app.
+        app. It is also used to avoid props drilling.
       </p>
       <div className="navbar-nav">
         <div>HOME</div>
