8f8993d6f23ebeb760925163983b0f13260b7b8f
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-13 13:59:03 +0530

    Update UserDetails.jsx

--- 18_user_table/src/components/UserDetails.jsx
+++ 18_user_table/src/components/UserDetails.jsx
@@ -1,27 +1,30 @@
 import { addUser } from "../store/slices/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import DeleteAllUser from "./DeleteAllUser";
+import { fakeUserData } from "../api";
 
 function UserDetails() {
   const dispatch = useDispatch();
+
+  const { users } = useSelector((store) => store.user);
+
+  const addNewUser = (name) => {
+    dispatch(addUser(name));
+    console.log(name);
+  };
+
   return (
     <div className="content">
       <div className="admin-table">
         <div className="admin-subtitle">List of User Details</div>
-        <button
-          className="addBtn"
-          onClick={() => {
-            dispatch(addUser("payload"));
-          }}
-        >
+        <button className="addBtn" onClick={() => addNewUser(fakeUserData())}>
           Add New Users
         </button>
       </div>
       <ul>
-        <li>Jay Shree Ram ✨</li>
-        <li>Jay Shree Krushna 💥</li>
-        <li>Jay Shree Hari</li>
-        <li>Jay Shree Dnyanoba Mauli Tukaram </li>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
       </ul>
       <hr
         style={{ height: "2px", margin: "10px 0", backgroundColor: "#fff" }}
