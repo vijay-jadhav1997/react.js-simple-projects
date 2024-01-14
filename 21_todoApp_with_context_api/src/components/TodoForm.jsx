00cde408b777c6bb2e5db85525cfc5e28770b69c
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-14 20:33:08 +0530

    Create TodoForm.jsx

---
+++ 21_todoApp_with_context_api/src/components/TodoForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import useTodo from "../contexts/todoContext";
+
+const TodoForm = () => {
+  const [todoTitle, setTodoTitle] = useState("");
+  const { addTodo } = useTodo();
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    if (todoTitle !== "") {
+      addTodo({ title: todoTitle, isCompleted: false });
+      setTodoTitle("");
+    } else {
+      alert("Please, enter todo title first..!");
+    }
+  };
+
+  return (
+    <form onSubmit={addTodoHandler} className="flex ">
+      <input
+        type="text"
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todoTitle}
+        onChange={(e) => setTodoTitle(e.target.value)}
+        placeholder="Enter your todo todoTitle here..."
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      ></button>
+    </form>
+  );
+};
+
+export default TodoForm;
