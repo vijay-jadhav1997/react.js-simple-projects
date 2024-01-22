b51caf5f18947d7083762e8ab61a3b9501162526
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-22 21:10:03 +0530

    start creating movie rating app

---
+++ 22_Movie_Rating_App/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
